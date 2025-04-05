cc4271364d0339bef468a26a2b1ed7293e5b26e2
ErikCaballeroh <erikaballero.hdz@gmail.com>
2025-04-05 14:04:29 -0600

    Implementacion de middlewares en las rutas y organizacion en exportaciones

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const express = require('express');
 const session = require('express-session');
 
 const app = express();
-const authRoutes = require('./routes/auth.routes');
-const clavesRoutes = require('./routes/claves.routes');
-const categoriasRoutes = require('./routes/categorias.routes');
-const familiasRoutes = require('./routes/familias.routes');
-const invitacionesRoutes = require('./routes/invitaciones.routes');
-const rolesRoutes = require('./routes/roles.routes');
-const usuariosRoutes = require('./routes/usuarios.routes');
 
-app.use(express.json());
+const {
+    authRoutes,
+    clavesRoutes,
+    categoriasRoutes,
+    familiasRoutes,
+    invitacionesRoutes,
+    rolesRoutes,
+    usuariosRoutes,
+} = require('./routes');
+
+const {
+    auth,
+    checkRole,
+} = require('./middlewares');
+
 app.use(express.json());
 
 app.use(session({
@@ -24,12 +31,12 @@ app.use(session({
 
 // Usar las rutas con el prefijo /api/ruta
 app.use('/api/auth', authRoutes);
-app.use('/api/claves', clavesRoutes);
-app.use('/api/categorias', categoriasRoutes);
-app.use('/api/familias', familiasRoutes);
-app.use('/api/invitaciones', invitacionesRoutes);
-app.use('/api/roles', rolesRoutes);
-app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/claves', auth, clavesRoutes);
+app.use('/api/categorias', auth, categoriasRoutes);
+app.use('/api/familias', auth, familiasRoutes);
+app.use('/api/invitaciones', auth, invitacionesRoutes);
+app.use('/api/roles', auth, rolesRoutes);
+app.use('/api/usuarios', auth, usuariosRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    authRoutes: require('./auth.routes'),
+    categoriasRoutes: require('./categorias.routes'),
+    clavesRoutes: require('./claves.routes'),
+    familiasRoutes: require('./familias.routes'),
+    invitacionesRoutes: require('./invitaciones.routes'),
+    rolesRoutes: require('./roles.routes'),
+    usuariosRoutes: require('./usuarios.routes'),
+}
